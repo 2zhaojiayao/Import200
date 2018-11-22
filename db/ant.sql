ed1f46109e4aaa15a33b50ea32635b5fc82e9c63
class2chengqiong <1538549768@qq.com>
2018-11-22 10:34:52 +0800

    建表sql语句

---
+++ db/ant.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50506
+Source Host           : localhost:3306
+Source Database       : ant
+
+Target Server Type    : MYSQL
+Target Server Version : 50506
+File Encoding         : 65001
+
+Date: 2018-11-22 10:20:56
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
