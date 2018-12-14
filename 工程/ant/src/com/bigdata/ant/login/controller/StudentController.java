504ec485f320989781c190a8cb922a3290c4c5da
lvninghui <1339757658@qq.com>
2018-12-14 22:56:01 +0800

    1

--- 工程/ant/src/com/bigdata/ant/login/controller/StudentController.java
+++ 工程/ant/src/com/bigdata/ant/login/controller/StudentController.java
@@ -37,8 +37,9 @@ public class StudentController {
 	 */
 	@RequestMapping("/stu_login")
 	public String Login(HttpServletRequest request) {
+		
 		HttpSession session = request.getSession();
-		String id = request.getParameter("id");
+		/*String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
 		String session_vcode = (String) session.getAttribute("text"); // 从session中获取真正的验证码
 		String form_vcode = request.getParameter("vcode"); // 获取用户输入的验证码
@@ -50,12 +51,12 @@ public class StudentController {
 		if (!(session_vcode.equalsIgnoreCase(form_vcode))) // 进行判断
 		{
 			request.setAttribute("msg", "验证码错误"); // 如果错误就将错误信息发送给客户端
-		}
-		boolean b = this.studentServiceImpl.FindIdAndPwd(id, pwd);
+		}*/
+		boolean b =true; /*this.studentServiceImpl.FindIdAndPwd(id, pwd);*/
 		if (b == false) {
 			return "three_login";
 		} else {
-			Student s = this.studentServiceImpl.FindName(id);
+			Student s = this.studentServiceImpl.FindName("2016011394");
 			session.setAttribute("s", s);
 			return "student_index";
 		}
