ab3f085f69a9c5939734bf274a9575044744a6f9
xujunmei <1649304921@qq.com>
2018-12-17 18:22:02 +0800

    1

---
+++ 工程/ant/WebContent/monitor_updateactivity.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>班委页面_修改活动</title>
+<script type="text/javascript" src="js/jquery.js"></script>
+<link type="text/css" rel="stylesheet"
+	href="css/monitor_updateactivity.css" />
+
+<script type="text/javascript" src="js/monitor_updateactivity.js"></script>
+<link type="text/css" rel="stylesheet"
+	href="css/monitor_updateactivitymin.css" />
+<link rel="stylesheet" type="text/css"
+	href="css/monitor_updateactivitytheme.css">
+</head>
+<body>
+	<!-- center -->
+	<div class="center">
+		<div class="center_text">点击以修改活动</div>
+		<div id="jsGrid" class="center_jsGrid jsgrid"></div>
+		<div class="center_submit">
+			<input type="submit" name="submit" value="提 交" class="button">
+		</div>
+	</div>
+	<script type="text/javascript" src="js/update.js"></script>
+</body>
+<script type="text/javascript"
+	src="https://cdn.bootcss.com/canvas-nest.js/1.0.1/canvas-nest.min.js"></script>
+</html>
---
+++ 工程/ant/src/com/bigdata/ant/login/service/MonitorServiceImpl.java
@@ -0,0 +1,66 @@
+package com.bigdata.ant.login.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bigdata.ant.login.dao.MonitorDaoImpl;
+
+/**
+ * 
+ * @ClassName:MonitorServiceImpl
+ * @Description:班委登录验证
+ * @Author xujunmei
+ * @Date:2018年12月12日
+ *
+ */
+@Service
+@Transactional(readOnly = false)
+public class MonitorServiceImpl {
+
+	@Resource
+	private MonitorDaoImpl monitorDaoImpl;
+
+	/**
+	 * 
+	 * @Title: getMonByIdAndPwd
+	 * @Description: 验证账号密码是否正确
+	 * @param:@param email
+	 * @param:@param pwd
+	 * @param:@return (参数)
+	 * @return:boolean(返回类型)
+	 *
+	 * @param email
+	 * @param pwd
+	 * @return
+	 */
+	public boolean getMonByEmailAndPwd(String email, String pwd) {
+		if (this.monitorDaoImpl.getMonByEmailAndPwd(email, pwd)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * 
+	 * @Title: getMonByEmail
+	 * @Description: TODO(这里用一句话描述这个方法的作用)
+	 * @param:@param email
+	 * @param:@param pwd
+	 * @param:@return (参数)
+	 * @return:boolean(返回类型)
+	 *
+	 * @param email
+	 * @param pwd
+	 * @return
+	 */
+	public boolean getMonByEmail(String email) {
+		if (this.monitorDaoImpl.getMonByEmail(email)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
