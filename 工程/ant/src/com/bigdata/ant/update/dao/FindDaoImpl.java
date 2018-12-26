161a5cf90c4d0cb99abd100919a83072e1f80900
xujunmei <1649304921@qq.com>
2018-12-26 18:50:53 +0800

    1

--- 工程/ant/src/com/bigdata/ant/update/dao/FindDaoImpl.java
+++
@@ -1,8 +0,0 @@
-package com.bigdata.ant.update.dao;
-
-import com.bigdata.ant.entity.Activity;
-import com.bigdata.ant.utils.BaseDao;
-
-public class FindDaoImpl extends BaseDao<Activity, Integer> {
-
-}
